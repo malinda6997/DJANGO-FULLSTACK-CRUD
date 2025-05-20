9448114e3fd5fe2a2e0eb761c6f62624ab43ef60
Malinda Prabath <malindaprabath876@gmail.com>
2025-05-20 20:12:12 +0530

    set up the server

--- CRUD_Project/CRUD_Project/settings.py
+++ CRUD_Project/CRUD_Project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'CRUD.apps.CRUDConfig,CrudConfig',  # Custom app for CRUD operations
 ]
 
 MIDDLEWARE = [
